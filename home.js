92173aa5ed33acf73738a1ffb7cd349c726acf74
dnrdls7 <dnrdls77@gmail.com>
2022-08-14 03:17:11 +0900

    homepage 수정

---
+++ home.js
@@ -0,0 +1,24 @@
+function currentTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    document.getElementById("timenow").innerHTML = hour + " : " + min + " : " + sec;
+}
+
+function liveTime() {
+    currentTime();
+    setInterval(currentTime, 1000);
+}
+
+window.addEventListener('load', liveTime)
+
+var audio = document.getElementById("bgm1");
+
+function bgmVol() {
+    audio.volume = 0.2;
+}
+
+setTimeout(() => bgmVol(), 500);
+setTimeout(() => bgmVol(), 1000);
